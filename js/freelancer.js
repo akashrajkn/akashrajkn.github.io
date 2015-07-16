9ddf984da040a2430d72d2e35c2ba415177c0c26
Akash Raj <akashrajkn@gmail.com>
2015-07-16 23:54:24 +0530

    comments removed/added

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,8 +1,4 @@
-/*!
- * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
+
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
