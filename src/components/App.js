71c090d638876ae688f9ecefe0d0852f724035e7
akashrajkn <akashrajkn@gmail.com>
2017-04-23 12:18:53 +0530

    1. Updated App component 2. Added babelrc and eslintrc files

--- src/components/App.js
+++ src/components/App.js
@@ -38,29 +38,7 @@ function App({ children, routes }) {
 
   return (
     <div style={s.root}>
-      <h1 style={s.title}>Single Page Apps for GitHub Pages</h1>
-      <Interactive
-        as="a"
-        href="https://github.com/rafrex/spa-github-pages"
-        style={s.repoLink}
-        {...s.link}
-      >https://github.com/rafrex/spa-github-pages</Interactive>
-      <nav style={s.mapMenu}>
-        {generateMapMenu()}
-      </nav>
-      {children}
-      <div style={s.creditLine}>
-        <Interactive
-          as="a"
-          href="http://www.rafaelpedicini.com"
-          interactiveChild
-          focus={{}}
-          touchActive={{}}
-          touchActiveTapOnly
-        >
-          Code and concept by <span {...s.childLink}>Rafael Pedicini</span>
-        </Interactive>
-      </div>
+      <h1 style={s.title}>Will be updated soon</h1>
     </div>
   );
 }
